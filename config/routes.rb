2f2a55eabb94f222984b9e070ee6d4c3199bea8d
Lamar Smith <Lamar5503@me.com>
2017-05-05 13:54:55 -0700

    route setups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  resource :styles
+  # get '/styles', to: 'styles#index'
+
+  get '/about', to: 'about#index'
+
+  get '/booking', to: 'booking#index'
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
