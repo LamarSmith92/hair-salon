6d325d28b4f31478b9a20b2cd4eac237e1e58526
Lamar Smith <Lamar5503@me.com>
2017-05-15 21:06:33 -0700

    cleaned up code, beautified and indented

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,10 +1,6 @@
 class AppointmentsController < ApplicationController
-
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
-
-
-
   def index
     @appointments = Appointment.where(user_id: current_user.id)
     if @appointments.length == 0
@@ -13,44 +9,38 @@ class AppointmentsController < ApplicationController
   end
 
   def new
-
     @appointment = Appointment.new
   end
 
   def create
-
-  @appointment = Appointment.new(appointment_params)
-  @appointment.user_id = current_user.id
-      if @appointment.save
-        flash[:success] = "Your Appointment has been set !"
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user.id
+    if @appointment.save
+      flash[:success] = "Your Appointment has been set !"
       redirect_to appointments_path
     else
-  render 'new'
-
-end
-
-end
+      render 'new'
+    end
+  end
 
   def show
     @appointments = Appointment.where(user_id: current_user.id)
-
   end
 
   def destroy
-      @appointment.destroy
-      respond_to do |format|
-        format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+    @appointment.destroy
+    respond_to do |format|
+      format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
+      format.json { head :no_content }
     end
+  end
 
-private
-
+  private
   def set_appointment
     @appointment = Appointment.find(params[:id])
   end
 
   def appointment_params
     params.require(:appointment).permit(:date, :first_name, :last_name, :phone, :email, :notes, )
-end
+  end
 end
