cd8ddf8a5f1ba49b60e55875743e48e5e7d85f35
Lamar Smith <Lamar5503@me.com>
2017-05-15 23:52:52 -0700

    bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :appointments
-  resources :clients
+
   devise_for :admins
   root 'home#index'
+  resources :appointments
   get '/about', to: 'about#index'
   get '/booking', to: 'booking#index'
   get '/styles', to: 'styles#index'
-  get '/appointments', to: 'appointments#index'
+
 
   devise_for :users do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
